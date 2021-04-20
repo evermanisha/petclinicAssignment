d4558bd9f1601b4818bbca65d1da24bb22c3758c
manishaj <manisha.jain@tesco.com>
2021-04-20 12:12:26 +0530

    jenkins infra TF files

---
+++ backend.tf
@@ -0,0 +1,9 @@
+#terraform {
+#  backend "s3" {
+#    bucket = "terraform-state-dishxl7m"
+#    key    = "terraform.tfstate"
+#    region = "eu-west-1"
+#  access_key = var.ACCESS_KEY
+#  secret_key = var.SECRET_KEY
+#  }
+#}
---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  region = var.AWS_REGION
+  access_key = var.ACCESS_KEY
+  secret_key = var.SECRET_KEY
+}
+
