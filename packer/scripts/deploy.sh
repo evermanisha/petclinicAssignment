6159b94bd64e4db5b5e13aa85a1a0625d2655e63
manishaj <manisha.jain@tesco.com>
2021-04-20 18:24:52 +0530

    unBuild Jar

--- packer/scripts/deploy.sh
+++ packer/scripts/deploy.sh
@@ -6,9 +6,9 @@ ssh-keyscan github.com >> ~/.ssh/known_hosts
 apt install -y maven
 apt install -y openjdk-11-jdk-headless
 
-git clone https://github.com/evermanisha/spring-petclinic-rest
+git clone https://github.com/evermanisha/spring-petclinic-rest.git
 cd spring-petclinic-rest
-mvn install
+#mvn install
 
 
 
